c155b785f82a92990192123b3d4a02347b6a74b2
Fareeha <fareeha.stars@gmail.com>
2022-09-21 16:55:53 -0700

    Added bubble sort feature and test

---
+++ src/BubbleSortTest.java
@@ -0,0 +1,12 @@
+import org.junit.*;
+
+public class BubbleSortTest {
+
+    @Test
+    public void bubbleSortTest() {
+        int[] arr = new int[]{5,8,2,4,0,1,7,9,3,6};
+        int[] arrCheck = new int[]{0,1,2,3,4,5,6,7,8,9};
+        Main.bubbleSort(arr);
+        Assert.assertArrayEquals(arrCheck,arr);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // bubble sort algorithm
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)(Math.random()*100); // random array of integers to sort
+        }
+        bubbleSort(arr);
+        for(int i = 0; i < arr.length; i++) { // print out sorted array
+            if (i != arr.length - 1) {
+                System.out.print(arr[i] + ", ");
+            } else {
+                System.out.print(arr[i]);
+            }
+        }
+    }
+
+    public static int[] bubbleSort(int[] arr) { // bubble sort method
+        int temp; // temporary int for swapping integers
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j+1]) { // check if j > j+1 swap for ascending order
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
