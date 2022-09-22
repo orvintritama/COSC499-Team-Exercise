7fad47611fae497d48c2002f9313b046cf90dbaa
Fareeha <fareeha.stars@gmail.com>
2022-09-21 19:51:28 -0700

    Moved bubble sort into correct directories and refactored code

--- COSC-499-Team-Exercise/src/BubbleSortTest.java
+++ COSC-499-Team-Exercise/src/BubbleSortTest.java
@@ -6,7 +6,7 @@ public class BubbleSortTest {
     public void bubbleSortTest() {
         int[] arr = new int[]{5,8,2,4,0,1,7,9,3,6};
         int[] arrCheck = new int[]{0,1,2,3,4,5,6,7,8,9};
-        Main.bubbleSort(arr);
+        BubbleSort.bubbleSort(arr);
         Assert.assertArrayEquals(arrCheck,arr);
     }
 }
