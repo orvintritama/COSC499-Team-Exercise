4eac826dbc8138f6e33beccb6d743cd9ef29267c
ryanlu-ubc <77523948+ryanlu-ubc@users.noreply.github.com>
2022-09-22 14:44:46 -0700

    Updated the test file
    deleted the insertion sort file

--- COSC-499-Team-Exercise/src/SortingTest.java
+++ COSC-499-Team-Exercise/src/SortingTest.java
@@ -1,14 +1,16 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.reflect.Array;
+
 import org.junit.jupiter.api.Test;
 
-class SelectionSortTest {
+class SortingTest {
 
 	@Test
 	void test() {
-		SelectionSort ss = new SelectionSort();
 		int[] arr = {62,48,10,25,19};
 		assertArrayEquals(new int[] {10,19,25,48,62}, SelectionSort.selectionSort(arr));
+                assertArrayEquals(new int[] {10,19,25,48,62}, InsertionSort.insertionSort(arr));
 	}
 
 }
